1671b68873d1c7bcd5cfa97fea477b4fa5d6ac8f
Gustavo <gustavotch@hotmail.com>
2021-04-20 16:27:51 -0300

    Logout button implementation and new color palette

--- app/src/main/java/com/example/minha_agenda/activity/LoginActivity.java
+++ app/src/main/java/com/example/minha_agenda/activity/LoginActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,9 +19,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
-import com.google.firebase.database.DatabaseReference;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -63,7 +59,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-
+    //Function to Validade user email and password
     private void validateLogin() {
 
         auth = FirebaseConfig.getFirebaseAuth();
--- app/src/main/java/com/example/minha_agenda/activity/MainActivity.java
+++ app/src/main/java/com/example/minha_agenda/activity/MainActivity.java
@@ -1,23 +1,54 @@
 package com.example.minha_agenda.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.minha_agenda.R;
+import com.example.minha_agenda.config.FirebaseConfig;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference firebaseReference = FirebaseDatabase.getInstance().getReference();
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseReference.child("teste").setValue(100);
+        auth = FirebaseConfig.getFirebaseAuth();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch ( item.getItemId() ) {
+            case R.id.logoutOption :
+                signOut();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void signOut() {
+        auth.signOut();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
     }
 }
